318e36e1b8ce656939a540f6530dca7cba30dd9f
xann11 <vitorhugomendesmenezes@gmail.com>
2026-05-28 23:00:33 -0300

    Deploy built Angular app to gh-pages branch

--- src/app/pages/index.ts
+++
@@ -1,6 +0,0 @@
-﻿export { HomePage } from './home-page.component';
-export { ProductsPage } from './products-page.component';
-export { OrderPage } from './order-page.component';
-export { PaymentPage } from './payment-page.component';
-export { AdminLoginPage } from './admin-login-page.component';
-export { AdminDashboardPage } from './admin-dashboard-page.component';
